ab68648acd2f235cf0f7b311e16380af2083bffa
Carlos Eduardo <142421895+CarlozEduardo@users.noreply.github.com>
2024-02-02 13:57:10 -0300

    acertando @path

--- rest-quarkus/src/main/java/com/rest/ProdutoResource.java
+++ rest-quarkus/src/main/java/com/rest/ProdutoResource.java
@@ -7,7 +7,7 @@ import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.Produces;
 
-@Path("/produtos")
+@Path("/hello")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class ProdutoResource {
